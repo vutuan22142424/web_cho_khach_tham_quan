6d4f74962ac6f3704a4f13745eadc2b2a2aba8ee
VuTuan <tuantgdd107@gmail.com>
2026-06-12 21:20:33 +0700

    fix: connect MongoDB Atlas

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,12 +1,31 @@
+// import mongoose from 'mongoose';
+
+// const MONGO_URI = 'mongodb://localhost:27017/Do_an_tot_nghiep';
+
+// let isConnected = false;
+
+// export async function connectDB() {
+//   if (isConnected) return;
+//   await mongoose.connect(MONGO_URI);
+//   isConnected = true;
+//   console.log('✅ MongoDB connected');
+// }
+
+
 import mongoose from 'mongoose';
 
-const MONGO_URI = 'mongodb://localhost:27017/Do_an_tot_nghiep';
+const MONGO_URI = process.env.MONGODB_URI!;
 
-let isConnected = false;
+let cached = (global as any).mongoose ?? { conn: null, promise: null };
+(global as any).mongoose = cached;
 
 export async function connectDB() {
-  if (isConnected) return;
-  await mongoose.connect(MONGO_URI);
-  isConnected = true;
-  console.log('✅ MongoDB connected');
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI, {
+      bufferCommands: false,
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
